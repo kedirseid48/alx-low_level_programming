7942a4b873c2d10864432be24cdf1b17471fc13e
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-17 16:51:44 -0800

    if else

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char n;
+	char b;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	for (b = 'A'; b <= 'Z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+        return (0);
+}
