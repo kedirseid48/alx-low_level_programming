765c44112d3b668136cab0a39fe6a52afac030c2
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-17 18:59:10 -0800

    hello

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of two digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c = '0';
+	int i;
+	int j;
+
+	while (c <= '9')
+	{
+		i = c + 1;
+		while (i <= '9')
+		{
+			j = i + 1;
+			while(j <= '9')
+			{
+				putchar(c);
+				putchar(i);
+				if (c != '7' || (c == '7' && i != '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				j++;
+			}
+			i++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
