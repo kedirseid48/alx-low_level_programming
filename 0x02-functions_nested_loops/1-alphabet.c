0525443d76013dcda233d474bf0fabd256b50269
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-21 12:52:01 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
