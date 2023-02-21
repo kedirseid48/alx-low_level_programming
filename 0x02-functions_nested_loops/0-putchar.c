911e9819fbf31fb74d6d9888193d19e8351ab275
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-21 11:55:37 -0800

    _putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main _putchar
+ *
+ * Return: 0
+ *
+ */
+int main() {
+char mytext[] = "_putchar";
+
+	for (int i = 0; i <= 7; i++)
+	{
+	putchar(mytext[i]);
+	}
+	putchar('\n');
+return 0;
+}
