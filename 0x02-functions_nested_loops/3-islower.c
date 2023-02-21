f144a68d7987c50fbbd4dd146b07a662b4f0aa63
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-21 14:31:38 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,16 @@
 /**
  * _islower - check character
  * Return: 0,1
+ * @c parameter number
  */
 int _islower(int c)
 {
-	if (c >=97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
