3987756946f5c1b30600f3c71a272a19bfe7f498
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-21 15:20:49 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - check character
+ * Return: 0,1,-1
+ * @n: a character argument
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('+');
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
