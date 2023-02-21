26c63ff558afab58e921961b66de04f31839566d
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-21 15:49:23 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - check character
+ * Return: b
+ * @x: a character argument
+ */
+int print_last_digit(int x)
+{
+	int b;
+
+	b = x % 10;
+	return (b);
+
+}
