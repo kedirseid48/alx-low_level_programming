5c638aa3649a6d1ebee7a9c820b59f32ef71ca71
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-21 13:58:16 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int a;
+	
+	for(a = 0, a < 10, a++)
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
