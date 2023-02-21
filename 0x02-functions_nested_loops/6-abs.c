ad0c9a2c6af6b6eea68098f92d61b6ae704625f1
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-21 15:33:55 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - check character
+ * Return: x,-x
+ * @x: a character argument
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
