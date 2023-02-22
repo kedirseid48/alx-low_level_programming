6f77473444579d80aa7acee7cef7cf4879132be6
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-21 23:38:25 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - multiple times table
+ */
+void times_table(void)
+{
+	int a, b, pro, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			pro = a * b;
+			if (pro >=10)
+			{
+				c = pro % 10;
+				d = pro / 10;
+				_putchar(d + '0');
+				_putchar(c + '0');
+			}
+			else if (pro < 10)
+			{
+				_putchar(pro + '0');
+			}
+			if (b != 9 && pro >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (b != 9 && pro < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+	}
+}
