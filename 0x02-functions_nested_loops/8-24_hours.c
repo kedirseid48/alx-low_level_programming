94348cd63d211def34eb8bc876dfde33f679e99b
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-21 17:34:06 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for(a = 0; a <= 2; a++)
+	for (a = 0; a <= 2; a++)
 	{
 		if (a == 1 || a == 0)
 		{
@@ -33,7 +33,7 @@ void jack_bauer(void)
 				for (c = 0; c <= 5; c++)
 				{
 					for (d = 0; d <= 9; d++)
-					{       
+					{
 						_putchar(a + '0');
 						_putchar(b + '0');
 						_putchar(':');
@@ -43,6 +43,6 @@ void jack_bauer(void)
 					}
 				}
 			}
-		}	
+		}
 	}
 }
