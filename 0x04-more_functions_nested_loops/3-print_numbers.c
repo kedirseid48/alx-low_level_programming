4fc6b8bcec26f757c04eeb395939fff7cc851670
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-23 13:55:29 -0800

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	char mynum[]="0123456789"
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 9)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(mynum[i]);
+		}
+	}
+}
