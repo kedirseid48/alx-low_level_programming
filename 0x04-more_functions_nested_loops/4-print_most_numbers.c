ee1338acd1c3f403e151d8ea09e6490e7e112358
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-24 12:12:17 -0800

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2)
+		{
+		{
+		else if (i == 4)
+		{
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
