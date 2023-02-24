96df03c2900aacc9d3451c4005cea0e7ece408a5
Kedir Seid Aman <kedirseid48@gmail.com>
2023-02-24 13:25:37 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_lines - print numbers from 0 to 9
+ */
+void print_lines(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < j; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
